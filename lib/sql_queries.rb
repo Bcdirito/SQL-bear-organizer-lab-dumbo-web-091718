c56ae1e9dbd74259bad3264fcf8d645ee069c5b3
Brian Di Rito <bdirito91@gmail.com>
2019-02-24 22:29:53 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,7 +1,7 @@
 def selects_all_female_bears_return_name_and_age
   "SELECT *
   from BEARS
-  where GENDER = 'F'"
+  where bears.gender = 'F'"
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
